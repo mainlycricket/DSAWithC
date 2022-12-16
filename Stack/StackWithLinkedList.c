23749e0591f38ed1517cff4d213d34958aaab874
mainlycricket <metusharjain@gmail.com>
2022-12-16 18:37:45 +0530

    Stack with Linked List

---
+++ Stack/StackWithLinkedList.c
@@ -0,0 +1,86 @@
+// Beginning of the linked list is considered as the TOP of the stack.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *top = NULL; // Global TOP pointer
+
+void push(int);
+int pop();
+void traverse();
+
+int main()
+{
+    int data;
+
+    push(10);
+    push(20);
+    push(30);
+
+    traverse();
+
+    data = pop();
+    printf("\nPopped Item: %d", data);
+
+    data = pop();
+    printf("\nPopped Item: %d", data);
+
+    traverse();
+
+    return 0;
+}
+
+void push(int data)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    if (temp == NULL)
+    {
+        printf("\nStack OVERFLOW!");
+        return;
+    }
+    temp->data = data;
+    temp->next = top;
+    top = temp;
+}
+
+int pop()
+{
+    int data;
+    struct node *temp;
+
+    if (top == NULL)
+    {
+        printf("\nStack UNDERFLOW!");
+        exit(1);
+    }
+
+    data = top->data;
+    temp = top;
+    top = top->next;
+
+    free(temp);
+    return data;
+}
+
+void traverse()
+{
+    struct node *temp = top;
+    printf("\nTraversing stack:");
+    if (temp == NULL)
+    {
+        printf("\nStack UNDERFLOW!");
+        return;
+    }
+
+    while (temp != NULL)
+    {
+        printf("\t%d", temp->data);
+        temp = temp->next;
+    }
+}
