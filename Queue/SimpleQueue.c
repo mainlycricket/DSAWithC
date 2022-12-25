568d19de99c97fbb5e07177117daaa3b330c5e97
Tushar <metusharjain@gmail.com>
2022-12-25 23:18:15 +0530

    SimpleQueue

---
+++ Queue/SimpleQueue.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+int queue[MAX], front = -1, rear = -1;
+
+void enqueue(int);
+int dequeue();
+void traverse();
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    traverse();
+
+    dequeue();
+    dequeue();
+    traverse();
+
+    return 0;
+}
+
+void enqueue(int data)
+{
+    if (rear == MAX - 1)
+    {
+        printf("\nQueue is full. Overflow!");
+        exit(1);
+    }
+
+    queue[++rear] = data;
+
+    if (front == -1)
+        front = 0;
+}
+
+int dequeue()
+{
+    if (front == -1)
+    {
+        printf("\nQueue is empty. Underflow!");
+        exit(1);
+    }
+
+    int data = queue[front];
+    queue[front] = 0;
+
+    if (front == rear)
+        front = rear = -1;
+    else
+        ++front;
+
+    return data;
+}
+
+void traverse()
+{
+    if (front == -1)
+    {
+        printf("\nUnderflow!");
+        exit(1);
+    }
+
+    printf("\nQueue: ");
+
+    for (int i = front; i <= rear; i++)
+        printf("%d ", queue[i]);
+}
